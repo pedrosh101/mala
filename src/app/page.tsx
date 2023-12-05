5e92609c514b762c38aa630fa67d8d7a5287af1c
pedrosh101 <pedro.sh101@gmail.com>
2023-12-05 17:57:03 -0300

    small-demo-css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white text-amber-800 text-2xl">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white text-amber-800 text-xl">
       <h1 className="font-harbour text-5xl">Mala</h1>
 
       <div className="relative h-96 w-96 mt-12 sm:mt-0">
@@ -13,7 +13,7 @@ export default function Home() {
           className="object-contain transition-opacity duration-700"
         />
       </div>
-      <div className="text-center cursor-pointer">
+      <div className="text-center cursor-pointer mt-8 sm:text-xl space-y-1">
         <h1>Artes Visuais</h1>
         <h1>Cinema</h1>
         <h1>Teatro</h1>
