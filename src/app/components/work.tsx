d12ab69448a445fd7c9e69a6431a6377e20870e9
pedrosh101 <pedro.sh101@gmail.com>
2024-04-20 08:42:05 -0300

    semomath

--- src/app/components/work.tsx
+++ src/app/components/work.tsx
@@ -5,10 +5,7 @@ function Work({ imageUrls }: any) {
   return (
     <main className="flex flex-col items-center min-h-screen bg-no-repeat bg-top text-white">
       <div
-        className={`grid grid-cols-${Math.min(
-          imageUrls.length,
-          3
-        )} gap-4 p-4 w-full`}
+        className="grid grid-cols-3 gap-4 p-4 w-full"
       >
         {imageUrls.map((imageUrl: any, index: any) => (
           <div key={index} className="relative min-h-[500px] w-full">
