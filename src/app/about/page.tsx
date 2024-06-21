fd7e2091d3544aad75e334fc52f0756d0752f159
pedrosh101 <pedro.sh101@gmail.com>
2024-06-21 15:20:24 -0300

    aboutDix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,148 +5,141 @@ function About() {
     <>
       <Navbar isProjetosPage={true} />
       <div className="flex w-full justify-center space-x-8 font-courier bg-slate-50">
-        <div className="mt-10 sm:mt-20 md:w-2/6 md:px-0 px-2 mb-20 text-black">
-          &nbsp;&nbsp;&nbsp;&nbsp;Matheus Malavazi, 1993, vulgo Mala, é um
-          multaartista ítalo-brasileiro nascido no interior de São Paulo,
-          mudou-se ainda jovem para o Rio de Janeiro, onde se dedicou
-          principalmente as artes Cênicas e ao Cinema até a pandemia em 2020.
-          Foi quando o desenho, a pintura e principalmente o Grafitti deixaram
-          de seu seu hobby e passaram a ser sua principal expressão artística
-          durante a quarentena. Mudou-se pra Europa para estudar Arte
-          Contemporânea onde se licenciou em Artes visuais pela Universidade do
-          Algarve (UALG), formou-se com grandes mestres como Rui Sanches,
-          Alexandre Barata (Xana), Pedro Cabral, Susana Medeiros e Sara Navarro,
-          além de ter realizado Erasmus em Málaga (ESP), na Faculdad de Bellas
-          Artes (UMA) e também estudou filosofia na Università degli Studi di
-          Bari Aldo Moro (UNIBA), em Bari, ITA.
-          <br />
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;A jornada de Mala na pintura reflete as
-          múltiplas experiências e influências que ele colecionou ao longo do
-          tempo. Mala, além de ser seu vulgo, é sua TAG nas ruas e, assim como
-          no grafitti - onde a tag é a essência, assim também é nas suas
-          pinturas. Sua obra é uma sinfonia de técnicas variadas, materiais
-          diversos e uma paleta de cores que transcende o convencional. Cada
-          pincelada é uma fusão de referências de lugares onde viveu e visitou,
-          resultando em um estilo próprio. É uma excêntrica fusão do movimento
-          vibrante e caótico das ruas, com a profundidade e a complexidade do
-          expressionismo abstrato. Seu processo criativo é muitas vezes um ato
-          de abstração total, onde o gesto e a automatização desempenham papéis
-          centrais. "Pintar sem pensar" onde a automação e a repetição da
-          escrita de seu TAG - é uma espécie de mantra, onde o artista cria uma
-          linguagem particular e um estilo próprio.
-          <br />
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;Cada tela, linho, ou qualquer outra superfície
-          é como se fosse um tubo de ensaio, onde a experimentação das mais
-          diversas cores, dos mais diversos materiais e superfícies fazem parte
-          de seu processo e da sua pesquisa artísticas. Suas pinturas são
-          narrativas visuais complexas que convidam o espectador a uma imersão
-          profunda, revelando camadas de significado que variam de acordo com a
-          perspectiva individual.
-          <br />
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;Essa é a sua principal pesquisa visual dentro
-          da pintura - mas não só.
-          <br />
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;Os "Cogumelos babilónicos"(ou "Cogumalas" em
-          português e "Mushuroomala" em inglês) é seu trabalhão mais figurativo
-          - são personagens que nasceram em paredes das ruas brasileiras e com o
-          confinamento da pandemia, passaram também para as telas. Assim como em
-          suas telas abstratas, abusa de uma vasta paleta de cores, de uma
-          mistura de matérias e superfícies, os cogumelos que parecem a primeira
-          vista ingénuos e até infantis, os quais se relacionam bastante com a
-          estética da Pop-Art, porém carregam sempre com uma mensagem
-          filosófica, existencial, psicológica e quase sempre uma crítica social
-          ... Até porque eles nasceram nas paredes da Babilônia.
-          <br />
-          <br />
-          <br />
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;Sua prática/pesquisa artística vai muito além
-          da Pintura... Matheus Malavazi é Ator e atuou nos palcos e nos sets de
-          filmagem nos 10 anos que antecederam a Pandemia. Fez teatro nos palcos
-          e principalmente nas ruas Cariocas, integrando o grupo "TáNaRua", do
-          grande mestre Amir Hadad.
-          <br />
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;Explora e pesquisa as possibilidades da
-          Performance, tendo realizada algumas em solo europeu, como COVID 19,
-          no final da Pandemia em Faro, PT, e "Mans don't cry" em Málaga, no sul
-          da Espanha.
-          <br />
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;No Cinema, sua janela de entrada na sétima
-          arte foi a atuação e, depois de realizar seu primeiro longa metragem,
-          se apaixonou pelo cinema e quis ir também para trás das câmeras.
-          Formou-se em Direção Cinematográfica pela Academia Internacional de
-          Cinema (AIC) no Rio de Janeiro.
-          <br />
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;Música e som sempre foi um fascínio desde
-          criança, mas a brincadeira começou a ficar seria quando começou a
-          estudar e fazer Cinema na Academia Internacional de Cinema (AIC), em
-          Botafogo, no Rio de Janeiro, onde estudou com o mestre do Som e da
-          Montagem cinematográfica Waldir Xavier. Logo em seguida
-          especializou-se em Montagem na EICTV, em Cuba. Posteriormente também
-          estudou produção musical na AIMEC, em Campinas-SP.
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          CV
-          <br />
-          <br />
-          2021_2024 - LICENCIATURA EM ARTES VISUAIS - Universidade do Algarve
-          (UALG)
-          <br />
-          <br />
-          2023 - Universidade Bellas Artes Málaga (UMA) - ERASMUS
-          <br />
-          <br />
-          2022 - Università degli studi di Bari Aldo Moro (Uniba) - ERASMUS
-          <br />
-          <br />
-          2020 - Workshop de Cinema com Lucrecia Martel - Academia internacional
-          de Cinema (AIC)
-          <br />
-          <br />
-          2020 - Produção musical - AIMEC
-          <br />
-          <br />
-          2018 - Taller montagem - Escuela Internacional de Cine y Televisión
-          (EICTV)
-          <br />
-          <br />
-          2016_17 - Direção cinematográfica - Academia internacional de Cinema
-          (AIC), Rio de Janeiro
-          <br />
-          <br />
-          2016 - Método Fátima Toledo - Studio Fátima Toledo, São Paulo
-          <br />
-          <br />
-          2013_15 - Atuação para Teatro, TV e cinema -Escola de Atores WolfMaya,
-          Rio de Janeiro/São Paulo
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          Exposições
-          <br />
-          <br />
-          2022 - Exposição Coletiva LOUD Galeria Arquente, Faro, PT Curadoria
-          Guilherme Gonçalves
-          <br />
-          <br />
-          2023 - Exposição Coletiva OURO AZUL IPDJ, Faro, PT Curadoria Luís
-          Marques e Ruben Gonçalves
-          <br />
-          <br />
-          2024 - Exposição Coletiva AMALGAMA Convento de Santo Antônio, Loulé,
-          PT Curadoria; Xana, Pedro Cabral e Susana Medeiros
+        <div className="mt-10 sm:mt-20 md:w-2/6 md:px-0 px-2 mb-20 text-black indent-8 space-y-8">
+          <h1>
+            Matheus Malavazi, 1993, vulgo Mala, é um multaartista
+            ítalo-brasileiro nascido no interior de São Paulo, mudou-se ainda
+            jovem para o Rio de Janeiro, onde se dedicou principalmente as artes
+            Cênicas e ao Cinema até a pandemia em 2020. Foi quando o desenho, a
+            pintura e principalmente o Grafitti deixaram de seu seu hobby e
+            passaram a ser sua principal expressão artística durante a
+            quarentena. Mudou-se pra Europa para estudar Arte Contemporânea onde
+            se licenciou em Artes visuais pela Universidade do Algarve (UALG),
+            formou-se com grandes mestres como Rui Sanches, Alexandre Barata
+            (Xana), Pedro Cabral, Susana Medeiros e Sara Navarro, além de ter
+            realizado Erasmus em Málaga (ESP), na Faculdad de Bellas Artes (UMA)
+            e também estudou filosofia na Università degli Studi di Bari Aldo
+            Moro (UNIBA), em Bari, ITA.
+          </h1>
+          <h1>
+            A jornada de Mala na pintura reflete as múltiplas experiências e
+            influências que ele colecionou ao longo do tempo. Mala, além de ser
+            seu vulgo, é sua TAG nas ruas e, assim como no grafitti - onde a tag
+            é a essência, assim também é nas suas pinturas. Sua obra é uma
+            sinfonia de técnicas variadas, materiais diversos e uma paleta de
+            cores que transcende o convencional. Cada pincelada é uma fusão de
+            referências de lugares onde viveu e visitou, resultando em um estilo
+            próprio. É uma excêntrica fusão do movimento vibrante e caótico das
+            ruas, com a profundidade e a complexidade do expressionismo
+            abstrato. Seu processo criativo é muitas vezes um ato de abstração
+            total, onde o gesto e a automatização desempenham papéis centrais.
+            "Pintar sem pensar" onde a automação e a repetição da escrita de seu
+            TAG - é uma espécie de mantra, onde o artista cria uma linguagem
+            particular e um estilo próprio.
+          </h1>
+          <h1>
+            {" "}
+            Cada tela, linho, ou qualquer outra superfície é como se fosse um
+            tubo de ensaio, onde a experimentação das mais diversas cores, dos
+            mais diversos materiais e superfícies fazem parte de seu processo e
+            da sua pesquisa artísticas. Suas pinturas são narrativas visuais
+            complexas que convidam o espectador a uma imersão profunda,
+            revelando camadas de significado que variam de acordo com a
+            perspectiva individual.
+          </h1>
+          <h1>
+            {" "}
+            Essa é a sua principal pesquisa visual dentro da pintura - mas não
+            só.
+          </h1>
+          <h1 className="pb-16">
+            {" "}
+            Os "Cogumelos babilónicos"(ou "Cogumalas" em português e
+            "Mushuroomala" em inglês) é seu trabalhão mais figurativo - são
+            personagens que nasceram em paredes das ruas brasileiras e com o
+            confinamento da pandemia, passaram também para as telas. Assim como
+            em suas telas abstratas, abusa de uma vasta paleta de cores, de uma
+            mistura de matérias e superfícies, os cogumelos que parecem a
+            primeira vista ingénuos e até infantis, os quais se relacionam
+            bastante com a estética da Pop-Art, porém carregam sempre com uma
+            mensagem filosófica, existencial, psicológica e quase sempre uma
+            crítica social ... Até porque eles nasceram nas paredes da
+            Babilônia.
+          </h1>
+          <h1>
+            Sua prática/pesquisa artística vai muito além da Pintura... Matheus
+            Malavazi é Ator e atuou nos palcos e nos sets de filmagem nos 10
+            anos que antecederam a Pandemia. Fez teatro nos palcos e
+            principalmente nas ruas Cariocas, integrando o grupo "TáNaRua", do
+            grande mestre Amir Hadad.
+          </h1>
+          <h1>
+            Explora e pesquisa as possibilidades da Performance, tendo realizada
+            algumas em solo europeu, como COVID 19, no final da Pandemia em
+            Faro, PT, e "Mans don't cry" em Málaga, no sul da Espanha.
+          </h1>
+          <h1>
+            {" "}
+            No Cinema, sua janela de entrada na sétima arte foi a atuação e,
+            depois de realizar seu primeiro longa metragem, se apaixonou pelo
+            cinema e quis ir também para trás das câmeras. Formou-se em Direção
+            Cinematográfica pela Academia Internacional de Cinema (AIC) no Rio
+            de Janeiro.
+          </h1>
+          <h1 className="pb-16">
+            Música e som sempre foi um fascínio desde criança, mas a brincadeira
+            começou a ficar seria quando começou a estudar e fazer Cinema na
+            Academia Internacional de Cinema (AIC), em Botafogo, no Rio de
+            Janeiro, onde estudou com o mestre do Som e da Montagem
+            cinematográfica Waldir Xavier. Logo em seguida especializou-se em
+            Montagem na EICTV, em Cuba. Posteriormente também estudou produção
+            musical na AIMEC, em Campinas-SP.
+          </h1>
+          <div className="indent-0 space-y-8">
+
+          <h1>CV</h1>
+          <h1>
+            {" "}
+            2021_2024 - LICENCIATURA EM ARTES VISUAIS - Universidade do Algarve
+            (UALG)
+          </h1>
+          <h1>2023 - Universidade Bellas Artes Málaga (UMA) - ERASMUS</h1>
+          <h1>
+            2022 - Università degli studi di Bari Aldo Moro (Uniba) - ERASMUS
+          </h1>
+          <h1>
+            2020 - Workshop de Cinema com Lucrecia Martel - Academia
+            internacional de Cinema (AIC)
+          </h1>
+          <h1>2020 - Produção musical - AIMEC</h1>
+          <h1>
+            2018 - Taller montagem - Escuela Internacional de Cine y Televisión
+            (EICTV)
+          </h1>
+          <h1>
+            2016_17 - Direção cinematográfica - Academia internacional de Cinema
+            (AIC), Rio de Janeiro
+          </h1>
+          <h1>2016 - Método Fátima Toledo - Studio Fátima Toledo, São Paulo</h1>
+          <h1 className="pb-16">
+            2013_15 - Atuação para Teatro, TV e cinema -Escola de Atores
+            WolfMaya, Rio de Janeiro/São Paulo
+          </h1>
+          <h1>Exposições</h1>
+
+          <h1>
+            2022 - Exposição Coletiva LOUD Galeria Arquente, Faro, PT Curadoria
+            Guilherme Gonçalves
+          </h1>
+          <h1>
+            2023 - Exposição Coletiva OURO AZUL IPDJ, Faro, PT Curadoria Luís
+            Marques e Ruben Gonçalves
+          </h1>
+          <h1>
+            2024 - Exposição Coletiva AMALGAMA Convento de Santo Antônio, Loulé,
+            PT Curadoria; Xana, Pedro Cabral e Susana Medeiros
+          </h1>
+          </div>
         </div>
       </div>
     </>
