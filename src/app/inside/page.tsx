9b89c2a3a8924e341a5c66c048a6ccc3b5522414
pedrosh101 <pedro.sh101@gmail.com>
2024-06-27 14:57:52 -0300

    macroBg

--- src/app/inside/page.tsx
+++ src/app/inside/page.tsx
@@ -81,18 +81,26 @@ function Inside() {
           : ""}
 
         {isModalOpen && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-90">
-            <div className="bg-white/20 p-2 rounded-lg h-[90%] w-[90%]">
+          <div className="fixed inset-0 flex items-center justify-center bg-black">
+            <div className=" p-2 h-[90%] w-[90%]">
               <button
-                className="absolute top-2 right-2 text-xl text-white font-realCourier"
+                className="absolute top-8 right-8 text-4xl text-white"
                 onClick={closeModal}
               >
-                x
+                <svg
+                  viewBox="0 0 1024 1024"
+                  fill="currentColor"
+                  height="1em"
+                  width="1em"
+                >
+                  <path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z" />
+                </svg>
               </button>
               <Swiper
                 spaceBetween={30}
                 slidesPerView={1}
                 navigation
+                loop={true}
                 pagination={{ clickable: true }}
                 modules={[Navigation, Pagination]}
               >
